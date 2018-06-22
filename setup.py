a0cb9bfc5d291ac01a7aab874c68d46ffc26992f
théo rostaing <rostaing.theo41@gmail.com>
2018-06-22 15:29:06 +0200

    restructuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+import distutils
+from distutils.core import setup, Extension
+
+import sdss3tools
+import os
+
+sdss3tools.setup(
+    name='ics_webViewer
+    description = "Toy SDSS-3 actor.",
+    )
+
